77ad3327521feb24e629aa5d49178971e56005b3
Brent Platt <bplatt92@live.com>
2023-08-22 21:30:26 -0400

    intro to functions lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,33 @@
 require ( './root.js' );
 
+function shout(string) {
+  return string.toUpperCase();
+}
 
-describe('shout(string)', function() {
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  string.toUpperCase();
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  string.toLowerCase();
+  console.log('hello')
+}
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string === 'hello')
+  return "I can\'t hear you!"
+  else if (string === 'HELLO')
+  return "YES INDEED!"
+  else (string === "Let\'s have dinner together!")
+  return "I would love to!"
+}
+
+describe("shout(string)", function () {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
   })
